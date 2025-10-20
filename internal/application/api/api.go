a2d921a05a6f9a51d5cd9340a8b80712371def9e
Atsuhiro <atcheri@gmail.com>
2025-10-20 13:31:10 +0200

    fix: error handling when several detailed errors are returned

--- internal/application/api/api.go
+++ internal/application/api/api.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"strings"
+
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -30,10 +32,19 @@ func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
 	paymentErr := a.payment.Charge(&order)
 
 	if paymentErr != nil {
-		st, _ := status.FromError(paymentErr)
+		st := status.Convert(paymentErr)
+		var allErrors []string
+		for _, detail := range st.Details() {
+			switch t := detail.(type) {
+			case *errdetails.BadRequest:
+				for _, violation := range t.GetFieldViolations() {
+					allErrors = append(allErrors, violation.Description)
+				}
+			}
+		}
 		fieldErr := &errdetails.BadRequest_FieldViolation{
 			Field:       "payment",
-			Description: st.Message(),
+			Description: strings.Join(allErrors, "\n"),
 		}
 		badReq := &errdetails.BadRequest{}
 		badReq.FieldViolations = append(badReq.FieldViolations, fieldErr)
